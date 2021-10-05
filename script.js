7c228bdc11e4b3c4a4fa31df4204eda21a4905e9
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2021-10-05 10:50:29 +0200

    Added script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+var $pr1 = document.getElementById('piece_red1');
+
+console.log($pr1);
+
+
+
+
+$(function()
+{
+  $('#piece_red1').click(function()
+                      {
+                    console.log('test');
+                    //$(this).animate({'left' : '75px'}, {duration : 400});
+                    $(this).css({top:'auto'});   
+                    $(this).animate({bottom:'485px'}, 400);
+                    
+                      });
+ });
