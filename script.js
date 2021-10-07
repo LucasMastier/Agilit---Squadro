23a705ab946eac631084fdfd064e3987c478e688
Lucas Mastier <73110352+LucasMastier@users.noreply.github.com>
2021-10-07 11:54:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var $pr1 = document.getElementById('piece_red1');
+var pr1Position = 0;
 
 console.log($pr1);
 
@@ -9,10 +10,11 @@ $(function()
 {
   $('#piece_red1').click(function()
                       {
-                    console.log('test');
-                    //$(this).animate({'left' : '75px'}, {duration : 400});
-                    $(this).css({top:'auto'});   
-                    $(this).animate({bottom:'485px'}, 400);
+                        console.log('test');
+                        //$(this).animate({'left' : '75px'}, {duration : 400});
+                        $(this).css({top:'auto'});   
+                        $(this).animate({bottom:'485px'}, 400);
+
                     
                       });
  });
