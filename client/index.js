c875916b60963f55e302be34c2e528c3ab6f99b6
84z2h <90423456+84z2h@users.noreply.github.com>
2022-01-12 17:29:28 +0100

    menu create (pas fini)

--- client/index.js
+++ client/index.js
@@ -1,16 +1,27 @@
-var list_game=["1234","azer"];
+var list_game=[];
 
 function popupJoin(){
+    /* fonction qui affiche le menu join 
+    */
     document.getElementById("join_container").style.display="flex";
-    document.getElementById("popup_submit").addEventListener('click',verify_code);
+    document.getElementById("btn_join").addEventListener('click',verify_code);
 }
 function popupCreate(){
+    /* fonction qui affiche le menu de creation */
     var code=makeid();
-    
+    document.getElementById("code_generated").innerHTML="Code de la partie : "+code;
+    document.getElementById("create_container").style.display="flex";
+    document.getElementById("btn_create").addEventListener('click',createRoom);
+}
+function createRoom(code,team){
+    /* creation de la partie */
+    list_game.push(code);
+    //le mettre sur une partie ou en attente 
 }
 function makeid() {
+    /* Genere un code aleatoire de longueur 4 en lettres et chiffre */
     var result           = '';
-    var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var characters       = '0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < 4; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * 
@@ -20,6 +31,8 @@ function makeid() {
 }
 //console.log(makeid());
 function verify_code(){
+    /* Verifie le code entre par l'user si le code existe ou pas 
+    */
     let finded=false;
     list_game.forEach(element => {
         if(element==document.getElementById("code").value){
